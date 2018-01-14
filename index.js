07ff3b6cc8558f9211d8cdee0aaa7e92f7ae01f7
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-14 03:26:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(location) {
-  if (location > 45) {
-    return location - 44;
-  } else if (location < 45)
-    return 44 - location;
+  if (location > 42) {
+    return location - 42;
+  } else if (location < 42)
+    return 42 - location;
 }
